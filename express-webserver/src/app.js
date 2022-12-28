e10f4dc0817affd149d39243a29e653113c228a5
Sincerely Brittany <sincerelybr@gmail.com>
2022-12-28 16:36:35 -0500

    serving static html pages in express

--- express-webserver/src/app.js
+++ express-webserver/src/app.js
@@ -1,9 +1,31 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+console.log(__dirname, "dirname");
+console.log(__filename, "filename");
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.send("Help Screen!");
+// });
+
+app.get("/weather", (req, res) => {
+  res.send("Weather Screen!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Screen!");
 });
 
 app.listen(port, () => {
