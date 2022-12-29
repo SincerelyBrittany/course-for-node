15119e526570deb7d2bbad69a0cf5c9cbbbeeaf3
Sincerely Brittany <sincerelybr@gmail.com>
2022-12-29 13:27:42 -0500

    adds error handling for /westher route

--- express-webserver/src/app.js
+++ express-webserver/src/app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 const app = express();
 const port = 3000;
 
@@ -8,27 +9,84 @@ console.log(__filename, "filename");
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //app.set is defined in https://www.npmjs.com/package/hbs
+//SetUp handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// app.get("/help", (req, res) => {
-//   res.send("Help Screen!");
-// });
-
-app.get("/weather", (req, res) => {
-  res.send("Weather Screen!");
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "SincerelyBrittany",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Screen!");
+  res.render("about", {
+    title: "About",
+    name: "SincerelyBrittany",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+
+  res.send({
+    forecast: forecastData,
+    location,
+    address: req.query.address,
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  console.log(req.query);
+  // Visit localhost:3000/products?search=games&rating=5
+  // You should see rating and games in terminal
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "SincerelyBrittany",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "SincerelyBrittany",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "SincerelyBrittany",
+    errorMessage: "Page not found.",
+  });
 });
 
 app.listen(port, () => {
