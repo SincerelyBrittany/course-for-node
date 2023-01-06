a83f6b2ca63f07564f23be03803bd20fd626d3dd
Sincerely Brittany <sincerelybr@gmail.com>
2023-01-06 06:01:07 -0500

    makes fetch request

--- express-webserver/public/js/app.js
+++ express-webserver/public/js/app.js
@@ -1,1 +1,22 @@
 console.log("client side jaavscript is loaded");
+
+// fetch("https://puzzle.mead.io/puzzle")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data.puzzle));
+
+// fetch("http://localhost:3000/weather?address=newyork")
+//   .then((response) => response.json())
+//   .then(({ forecast, location, address } = {}) =>
+//     console.log(forecast, location, address)
+//   );
+
+fetch("http://localhost:3000/weather?address=newyork")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.error) {
+      console.log(error);
+    } else {
+      const { forecast, location, address } = data;
+      console.log(forecast, location, address);
+    }
+  });
